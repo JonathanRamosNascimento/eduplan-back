685ec4e8f8ed4d23cab08cc1fc95d99a94d4404e
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-10-15 19:01:21 -0300

    adicionando o lomobok na classe User do Security

--- src/main/java/com/unievangelica/eduplan/api/security/model/CurrentUser.java
+++ src/main/java/com/unievangelica/eduplan/api/security/model/CurrentUser.java
@@ -1,7 +1,11 @@
 package com.unievangelica.eduplan.api.security.model;
 
 import com.unievangelica.eduplan.api.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class CurrentUser {
 	
 	private String token;
@@ -9,22 +13,6 @@ public class CurrentUser {
 
 	public CurrentUser(String token, User user) {
 		this.token = token;
-		this.user = user;
-	}
-
-	public String getToken() {
-		return token;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
 		this.user = user;
 	}
 }
