11601bafb274c0724493a2c54175446f0532acb5
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-06-10 08:34:40 -0300

    alterando a entidade subject para referenciar o user e o plano de ensino

--- src/main/java/com/unievangelica/eduplan/api/entity/PlanoDeEnsino.java
+++ src/main/java/com/unievangelica/eduplan/api/entity/PlanoDeEnsino.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,12 +17,13 @@ public class PlanoDeEnsino {
 	@Id
 	private String id;
 
-	@DBRef(lazy = true)
+	@DBRef
 	private User user;
 
 	private Date data;
 
-	private String disciplina;
+	@DBRef
+	private Subject disciplina;
 
 	private String turno;
 
@@ -30,34 +32,4 @@ public class PlanoDeEnsino {
 	private String ano;
 
 	private String semestre;
-
-	private String chtotal;
-
-	private String chteorica;
-
-	private String chpratica;
-
-	private String ementa;
-
-	private String objetivoGeral;
-
-	private String objetivoEspecifico;
-
-	private String habilidadeCompetencias;
-
-	private String conteudoProgramatico;
-
-	private String procedimentosDidaticos;
-
-	private String atividadeIntegrativa;
-
-	private String primeiraVA;
-
-	private String segundaVA;
-
-	private String terceiraVA;
-
-	private String bibliografiaBasica;
-
-	private String bibliografiaComplementar;
 }
